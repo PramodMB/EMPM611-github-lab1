8b679ac594568d8864a16be447153a6f02873bdd
Pramod Mallenahalli Basavarajappa <pramodmb@umd.edu>
2024-09-16 22:18:08 -0400

    search function implement ENPM611

--- search.py
+++ search.py
@@ -1,16 +1,6 @@
 import json
 
 def search_json(json_data, search_string):
-    """
-    Search for a user in the JSON data by the 'User' field.
-
-    Args:
-        json_data (list): The loaded JSON data as a list of dictionaries.
-        search_string (str): The user string to search for in the JSON data.
-
-    Returns:
-        dict: A dictionary containing the matched user's data or an empty list if no match is found.
-    """
     # Loop through each item (dictionary) in the JSON data
     for item in json_data:
         # Check if the 'User' field matches the search string
